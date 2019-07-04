9503ebfbea0774962c33221e184567a6b7c1a985
Ryan Alexander <ryandalex1@outlook.com>
2019-07-03 22:07:06 -0500

    Move instantiation of DifferentialDrive to command instead of in subsystem to hopefully avoid motor warnings.

--- src/main/java/com/team2502/disco/command/teleop/JoystickDriveCommand.java
+++ src/main/java/com/team2502/disco/command/teleop/JoystickDriveCommand.java
@@ -3,18 +3,22 @@ package com.team2502.disco.command.teleop;
 import com.team2502.disco.OI;
 import com.team2502.disco.Robot;
 import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
 public class JoystickDriveCommand extends Command
 {
+    private final DifferentialDrive drive;
+
     public JoystickDriveCommand()
     {
         requires(Robot.DRIVE_TRAIN);
+        drive = new DifferentialDrive(Robot.DRIVE_TRAIN.left, Robot.DRIVE_TRAIN.right);
     }
 
     @Override
     protected void execute()
     {
-        Robot.DRIVE_TRAIN.getDrive().tankDrive(-OI.JOYSTICK_DRIVE_LEFT.getY(), OI.JOYSTICK_DRIVE_RIGHT.getY(),true);
+        drive.tankDrive(-OI.JOYSTICK_DRIVE_LEFT.getY(), OI.JOYSTICK_DRIVE_RIGHT.getY(),true);
     }
 
     @Override
--- src/main/java/com/team2502/disco/subsystem/DriveTrainSubsystem.java
+++ src/main/java/com/team2502/disco/subsystem/DriveTrainSubsystem.java
@@ -36,6 +36,7 @@ public class DriveTrainSubsystem extends Subsystem
 
     }
 
+
     @Override
     protected void initDefaultCommand()
     {
